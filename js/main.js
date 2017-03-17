cae3c09335cac98b336d963cbf3c0a7d16418524
Win <win.m179@gmail.com>
2017-03-17 20:19:55 +0000

    added a function to add task in main.js and added task is stored to the localStorage

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@ $(document).ready(function() {
 		addTask(e);
 	});
 
+
+
 	// Function adding task
 	function addTask(e) {
+
+		// localStorage.removeItem('tasks');
+		// Add unique id
+		var newDate = new Date();
+		var id = newDate.getTime();
 		var task = $('#task').val();
 		var taskPriority = $('#priority').val();
 		var taskDate = $('#date').val();
 		var taskTime = $('#time').val();
 
-		// Validations
+		// Validation
 		if (task == '') {
 			alert('Task can not be empty!');
 			e.preventDefault();
-		} else if (taskPriority == '' || taskPriority == 'Low') {
-			e.preventDefault();
+		} else if (taskPriority == '') {
+			taskPriority == 'Low';
 		} else if (taskDate == '') {
 			alert('Please select date!');
 			e.preventDefault();
@@ -25,6 +32,32 @@ $(document).ready(function() {
 			e.preventDefault();
 		} else {
 
+			// First check to see if there any tasks available
+			// I want it in object format so i used JSON.parse to convert tasks into object
+			var tasks = JSON.parse(localStorage.getItem('tasks'));
+
+			// Check if there is any tasks
+			// If not, set it to empty array
+			if (tasks == null) {
+				tasks = [];
+			}
+
+			// Take all value passed down from form.submit and 
+			// put them in a newTask object
+			var newTask = {
+				'id' : id,
+				'task' : task,
+				'taskPriority' : taskPriority,
+				'taskDate' : taskDate,
+				'taskTime' : taskTime
+			}
+
+			tasks.push(newTask);
+
+			// now it's ready to store added tasks into the localStorage object
+			// localStorage only stores string
+			// So i used JSON.stringify
+			localStorage.setItem('tasks', JSON.stringify(tasks));
 		}
 	}
 });
